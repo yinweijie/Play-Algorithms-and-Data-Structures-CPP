e1e5206ba1bd28cdce65cfa14c635e7b0c234e81
yinweijie <ywj123450@gmail.com>
2020-10-25 00:06:17 +0800

    InsertionSort的外层循环改为从0开始，保证输入数组长度为1时排序算法可用

--- 04-Insertion-Sort/02-Insertion-Sort/InsertionSort.h
+++ 04-Insertion-Sort/02-Insertion-Sort/InsertionSort.h
@@ -17,14 +17,14 @@ public:
     template <typename T>
     static void sort(T arr[], int n)
     {
-        for(int i = 1; i < n; i++)
+        for(int i = 0; i < n; i++)
         {
             // 将 arr[i] 插入到合适的位置
             for(int j = i; j > 0 && arr[j] < arr[j - 1]; j--)
             {
                 swap(arr, j, j - 1);
             }
-            // for(int j = i; j > 0; j--)
+            // for(int j = 0; j > 0; j--)
             // {
             //     if(arr[j] < arr[j - 1])
             //     {
--- 04-Insertion-Sort/03-Insertion-Sort-Optimized/InsertionSort.h
+++ 04-Insertion-Sort/03-Insertion-Sort-Optimized/InsertionSort.h
@@ -17,7 +17,7 @@ public:
     template <typename T>
     static void sort(T arr[], int n)
     {
-        for(int i = 1; i < n; i++)
+        for(int i = 0; i < n; i++)
         {
             // 将 arr[i] 插入到合适的位置
             for(int j = i; j > 0 && arr[j] < arr[j - 1]; j--)
@@ -40,7 +40,7 @@ public:
     template <typename T>
     static void sort2(T arr[], int n)
     {
-        for(int i = 1; i < n; i++)
+        for(int i = 0; i < n; i++)
         {
             // 将 arr[i] 插入到合适的位置
             T t = arr[i];
