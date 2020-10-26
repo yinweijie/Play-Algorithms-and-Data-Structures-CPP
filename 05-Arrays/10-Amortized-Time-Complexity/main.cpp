3ab7cc4757ecdfc7eee5d7f0a88b6b74913fc39a
yinweijie <ywj123450@gmail.com>
2020-10-27 01:46:22 +0800

    0510 completed: remove() updated

---
+++ 05-Arrays/10-Amortized-Time-Complexity/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Array.h"
+
+using namespace std;
+
+int main()
+{
+    Array<int> arr(10);
+
+    for(int i = 0; i < 10; i++)
+    {
+        arr.addLast(i);
+    }
+    cout << arr << endl;
+
+    arr.add(1, 100);
+    cout << arr << endl;
+
+    arr.addFirst(-1);
+    cout << arr << endl;
+
+    arr.remove(2);
+    cout << arr << endl;
+
+    arr.removeElement(4);
+    cout << arr << endl;
+
+    arr.removeFirst();
+    cout << arr << endl;
+
+    return 0;
+}
