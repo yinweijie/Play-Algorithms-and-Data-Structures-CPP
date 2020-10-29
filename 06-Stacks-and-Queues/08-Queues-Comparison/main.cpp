8b746f0ab8850e92bd0f1a82452be3dfe7ac5528
yinweijie <ywj123450@gmail.com>
2020-10-29 19:48:57 +0800

    0608 completed

---
+++ 06-Stacks-and-Queues/08-Queues-Comparison/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <limits>
+#include "LoopQueue.h"
+#include "ArrayQueue.h"
+
+using namespace std;
+
+double testQueue(Queue<int>& q, int opCount)
+{
+    auto startTime = std::chrono::steady_clock::now();
+
+    std::uniform_int_distribution<int> u(0, std::numeric_limits<int>::max());
+    std::default_random_engine e(time(0));
+
+    for(int i = 0; i < opCount; ++i)
+    {
+        q.enqueue(u(e));
+    }
+    for(int i = 0; i < opCount; ++i)
+    {
+        q.dequeue();
+    }
+
+    auto endTime = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time = endTime - startTime;
+
+    return time.count();
+}
+
+int main()
+{
+    int opCount = 100000;
+
+    ArrayQueue<int> arrayQueue;
+    double time1 = testQueue(arrayQueue, opCount);
+    cout << "ArrayQueue, time: " << time1 << " s" << endl;
+
+    LoopQueue<int> loopQueue;
+    double time2 = testQueue(loopQueue, opCount);
+    cout << "LoopQueue, time: " << time2 << " s" << endl;
+
+    return 0;
+}
