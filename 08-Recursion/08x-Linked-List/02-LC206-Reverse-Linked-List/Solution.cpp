e551164657b8eabce1c9e14ca1934a3723b5c299
yinweijie <ywj123450@gmail.com>
2020-11-07 16:49:57 +0800

    0808x completed

---
+++ 08-Recursion/08x-Linked-List/02-LC206-Reverse-Linked-List/Solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        ListNode* cur = head;
+
+        while(cur)
+        {
+            ListNode* next = cur->next;
+            cur->next = pre;
+
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+};
