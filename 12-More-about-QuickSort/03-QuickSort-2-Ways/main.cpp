1fdb9fba038808681d49fe38bae4f663ef3ac6af
yinweijie <ywj123450@gmail.com>
2020-11-13 22:30:23 +0800

    1203 completed

---
+++ 12-More-about-QuickSort/03-QuickSort-2-Ways/main.cpp
@@ -0,0 +1,47 @@
+#include <array>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include "ArrayGenerator.h"
+#include "SortingHelper.h"
+#include "MergeSort.h"
+#include "QuickSort.h"
+
+using namespace std;
+
+int main()
+{
+    int n = 100000;
+
+    int* arr = ArrayGenerator::generateRandomArray(n, n);
+    int* arr2 = new int[n];
+    std::copy(arr, arr + n, arr2);
+
+    cout << "Random Array" << endl;
+    SortingHelper::sortTest("QuickSort", arr, n);
+    SortingHelper::sortTest("QuickSort2Ways", arr, n);
+    delete[] arr;
+    delete[] arr2;
+
+    arr = ArrayGenerator::generateOrderedArray(n);
+    arr2 = new int[n];
+    std::copy(arr, arr + n, arr2);
+
+    cout << "Ordered Array" << endl;
+    SortingHelper::sortTest("QuickSort", arr, n);
+    SortingHelper::sortTest("QuickSort2Ways", arr, n);
+    delete[] arr;
+    delete[] arr2;
+
+    arr = ArrayGenerator::generateRandomArray(n, 1);
+    arr2 = new int[n];
+    std::copy(arr, arr + n, arr2);
+
+    cout << "Same Value Array" << endl;
+    SortingHelper::sortTest("QuickSort", arr, n);
+    SortingHelper::sortTest("QuickSort2Ways", arr, n);
+    delete[] arr;
+    delete[] arr2;
+
+    return 0;
+}
