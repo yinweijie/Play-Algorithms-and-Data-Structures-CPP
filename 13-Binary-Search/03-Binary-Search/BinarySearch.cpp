78e3d001bba1765e19ee12d61cbfeeb00aafad5b
yinweijie <ywj123450@gmail.com>
2020-11-16 21:37:48 +0800

    1303 completed

--- 13-Binary-Search/03-Binary-Search/BinarySearch.cpp
+++ 13-Binary-Search/03-Binary-Search/BinarySearch.cpp
@@ -9,6 +9,7 @@ public:
     {
         int l = 0, r = n - 1;
 
+        // 在 data[l, r] 的范围中查找 target
         while(l <= r)
         {
             int mid = l + (r - l) / 2;
@@ -24,5 +25,27 @@ public:
                 r = mid - 1;
             }
         }
+    }
+
+public:
+    template <typename T>
+    static int searchR(T data[], T target, int n)
+    {
+        return searchR(data, 0, n - 1, target);
+    }
+
+private:
+    template <typename T>
+    static int searchR(T data[], int l, int r, T target)
+    {
+        if(l > r) return -1;
+
+        int mid = l + (r - l) / 2;
+
+        if(data[mid] == target) return mid;
+
+        if(data[mid] < target) return searchR(data, mid + 1, r, target);
+
+        return searchR(data, l, mid - 1, target);
     }
 };
