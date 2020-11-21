c5c16c39f721561f9b63e5f218bae277801b3e59
yinweijie <ywj123450@gmail.com>
2020-11-21 19:12:15 +0800

    1507 completed

---
+++ 15-Binary-Search-Tree/07-Search-in-BST/BST.h
@@ -0,0 +1,153 @@
+#ifndef __BST_H__
+#define __BST_H__
+
+template <typename T>
+class BST
+{
+private:
+    struct Node
+    {
+        T e;
+        Node* left;
+        Node* right;
+
+        Node(T e)
+        {
+            this->e = e;
+            left = nullptr;
+            right = nullptr;
+        }
+
+        ~Node()
+        {
+            if(left != nullptr) delete left;
+            if(right != nullptr) delete right;
+        }
+    };
+
+    Node* root;
+    int size;
+
+public:
+    BST()
+    {
+        root = nullptr;
+        size = 0;
+    }
+
+    ~BST()
+    {
+        delete root;
+        size = 0;
+    }
+
+    int size()
+    {
+        return size;
+    }
+
+    bool isEmpty()
+    {
+        return size == 0;
+    }
+
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后二分搜索树的根
+    void add(T e)
+    {
+        root = add(root, e);
+    }
+
+private:
+    Node* add(Node* node, T e)
+    {
+        if(node == nullptr)
+        {
+            m_size++;
+            return new Node(e);
+        }
+
+        if(e > node->e)
+        {
+            node->right = add(node->right, e);
+        }
+        else if(e < node->e)
+        {
+            node->left = add(node->left, e);
+        }
+
+        return node;
+    }
+
+public:
+    void add2(T e)
+    {
+        if(root == nullptr)
+        {
+            root = new Node(e);
+            m_size++;
+            return;
+        }
+
+        Node* p = root;
+        while(p != nullptr)
+        {
+            if(e < p->e)
+            {
+                if(p->left == nullptr)
+                {
+                    p->left = new Node(e);
+                    m_size++;
+                    return;
+                }
+
+                p = p->left;
+            }
+            else if(e > p->e)
+            {
+                if(p->right == nullptr)
+                {
+                    p->right = new Node(e);
+                    m_size++;
+                    return;
+                }
+
+                p = p->right;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+public:
+    bool contains(T e)
+    {
+        return contains(root, e);
+    }
+
+private:
+    bool contains(Node* node, T e)
+    {
+        if(node == nullptr)
+        {
+            return false;
+        }
+
+        if(e == node->e)
+        {
+            return true;
+        }
+        else if(e < node->e)
+        {
+            return contains(node->left, e);
+        }
+        else
+        {
+            return contains(node->right, e);
+        }
+    }
+};
+
+#endif
