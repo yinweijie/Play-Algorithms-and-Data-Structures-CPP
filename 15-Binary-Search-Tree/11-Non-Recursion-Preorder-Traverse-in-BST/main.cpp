3401ef1bafa4cff65c23567c1acf8a5bb16dca8d
yinweijie <ywj123450@gmail.com>
2020-11-22 23:01:52 +0800

    1511 completed

---
+++ 15-Binary-Search-Tree/11-Non-Recursion-Preorder-Traverse-in-BST/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include "BST.h"
+
+using namespace std;
+
+int main()
+{
+    BST<int> bst;
+    vector<int> nums = {5, 3, 6, 8, 4, 2};
+    for(auto num: nums)
+    {
+        bst.add(num);
+    }
+
+    /////////////////
+    //      5      //
+    //    /   \    //
+    //   3    6    //
+    //  / \    \   //
+    // 2  4     8  //
+    /////////////////
+
+    bst.preOrder();
+    cout << endl;
+    bst.preOrderNR();
+    cout << endl;
+
+    // bst.inOrder();
+    // cout << endl;
+
+    // bst.postOrder();
+    // cout << endl;
+
+    return 0;
+}
