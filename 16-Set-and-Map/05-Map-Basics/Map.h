00e56d484494da615a379ffa8185a94ea8b83c9c
yinweijie <ywj123450@gmail.com>
2020-11-26 01:00:43 +0800

    1605 completed

--- 16-Set-and-Map/05-Map-Basics/Map.h
+++ 16-Set-and-Map/05-Map-Basics/Map.h
@@ -5,13 +5,13 @@ template <typename K, typename V>
 class Map
 {
 public:
-    virtual add(K key, V value) = 0;
+    virtual void add(K key, V value) = 0;
     virtual V remove(K key) = 0;
-    bool contains(K key) = 0;
-    V get(K key) = 0;
-    void set(K key, V newValue) = 0;
-    int getSize() = 0;
-    bool isEmpty() = 0;
+    virtual bool contains(K key) = 0;
+    virtual V get(K key) = 0;
+    virtual void set(K key, V newValue) = 0;
+    virtual int getSize() = 0;
+    virtual bool isEmpty() = 0;
 };
 
 #endif
