2498c316ad49f1257ea79d26ce9fd147d5d9dda6
yinweijie <ywj123450@gmail.com>
2020-11-27 21:40:13 +0800

    1602 completed

--- 16-Set-and-Map/02-LinkedListSet/main.cpp
+++ 16-Set-and-Map/02-LinkedListSet/main.cpp
@@ -3,9 +3,19 @@
 #include <vector>
 #include <string>
 #include <regex>
+#include <cctype>
 #include "LinkedListSet.h"
 
 using namespace std;
+
+// 参考：https://en.cppreference.com/w/cpp/string/byte/tolower
+std::string str_tolower(std::string s)
+{
+    std::transform(s.begin(), s.end(), s.begin(), 
+                   [](unsigned char c){ return std::tolower(c); }
+                  );
+    return s;
+}
 
 void calcWords(string str)
 {
@@ -21,8 +31,9 @@ void calcWords(string str)
         char* token = strtok(const_cast<char*>(text.data()), " ");
         while(token != NULL)
         {
-            words.push_back(token);
-            set.add(token);
+            string word = str_tolower(token);
+            words.push_back(word);
+            set.add(word);
             token = strtok(NULL, " ");
         }
     }
