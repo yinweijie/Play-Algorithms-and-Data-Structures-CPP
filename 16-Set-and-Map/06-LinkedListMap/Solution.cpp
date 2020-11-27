6f22874295ae34fdd2f776ffee2889f7e43b99a1
yinweijie <ywj123450@gmail.com>
2020-11-27 22:53:42 +0800

    1606 completed

--- 16-Set-and-Map/06-LinkedListMap/Solution.cpp
+++ 16-Set-and-Map/06-LinkedListMap/Solution.cpp
@@ -1,10 +1,29 @@
-#ifndef __LINKEDLISTMAP_H__
-#define __LINKEDLISTMAP_H__
+/// Leetcode 350. Intersection of Two Arrays II
+/// https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+///
+/// 课程中在这里暂时没有介绍这个问题
+/// 该代码主要用于使用Leetcode上的问题测试我们的LinkedListMap类
 
+#include <vector>
 #include <iostream>
-#include "Map.h"
 
 using namespace std;
+
+class Solution {
+private:
+
+template <typename K, typename V>
+class Map
+{
+public:
+    virtual void add(K key, V value) = 0;
+    virtual V remove(K key) = 0;
+    virtual bool contains(K key) = 0;
+    virtual V get(K key) = 0;
+    virtual void set(K key, V newValue) = 0;
+    virtual int getSize() = 0;
+    virtual bool isEmpty() = 0;
+};
 
 template <typename K, typename V>
 class LinkedListMap : public Map<K, V>
@@ -100,7 +119,8 @@ public:
         }
         else
         {
-            throw std::invalid_argument("Key :" + string(key) + " does not exist.");
+            // throw std::invalid_argument("Key :" + string(key) + " does not exist.");
+            return 0;
         }
     }
 
@@ -130,7 +150,8 @@ public:
         Node* node = getNode(key);
         if(node == nullptr)
         {
-            throw std::invalid_argument("Key: " + string(key) + " does not exist.");
+            // throw std::invalid_argument("Key: " + string(key) + " does not exist.");
+            return 0;
         }
         return node->Value;
     }
@@ -140,7 +161,8 @@ public:
         Node* node = getNode(key);
         if(node == nullptr)
         {
-            throw std::invalid_argument("Key: " + string(key) + " does not exist.");
+            // throw std::invalid_argument("Key: " + string(key) + " does not exist.");
+            return;
         }
         node->Value = newValue;
     }
@@ -157,4 +179,39 @@ public:
     
 };
 
-#endif
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        LinkedListMap<int, int> map;
+        for(int num: nums1){
+            if(!map.contains(num))
+                map.add(num, 1);
+            else
+                map.set(num, map.get(num) + 1);
+        }
+
+        vector<int> res;
+        for(int num: nums2){
+            if(map.contains(num)){
+                res.push_back(num);
+                map.set(num, map.get(num) - 1);
+                if(map.get(num) == 0)
+                    map.remove(num);
+            }
+        }
+
+        vector<int> ret;
+        ret.resize(res.size());
+        for(int i = 0 ; i < res.size() ; i ++)
+            ret[i] = res[i];
+
+        return ret;
+    }
+};
+
+int main()
+{
+    vector<int> nums1 = {4,9,5};
+    vector<int> nums2 = {9,4,9,8,4};
+    vector<int> res = Solution().intersect(nums1, nums2);
+    return 0;
+}
