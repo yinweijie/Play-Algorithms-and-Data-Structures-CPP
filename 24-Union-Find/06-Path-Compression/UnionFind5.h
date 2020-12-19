46d5c0d60cab286bf032682f18b482090ca491f3
yinweijie <ywj123450@gmail.com>
2020-12-19 16:45:37 +0800

    2406 completed

---
+++ 24-Union-Find/06-Path-Compression/UnionFind5.h
@@ -0,0 +1,91 @@
+#ifndef __UNIONFIND5_H__
+#define __UNIONFIND5_H__
+
+#include <vector>
+#include <stdexcept>
+#include "UF.h"
+
+using std::vector;
+
+// 我们的第四版Union-Find
+class UnionFind5 : public UF
+{
+private:
+    vector<int> parent;  // parent[i]表示第一个元素所指向的父节点
+    vector<int> rank;    // parent[i]表示第i个元素所指向的父节点
+
+    // 查找过程, 查找元素p所对应的集合编号
+    // O(h)复杂度, h为树的高度
+    int find(int p)
+    {
+        if(p < 0 || p >= parent.size())
+        {
+            throw std::invalid_argument("p is out of bound");
+        }
+
+        while(p != parent[p])
+        {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+
+        return p;
+    }
+public:
+    // 构造函数
+    UnionFind5(int size)
+    {
+        parent.reserve(size);
+        rank.reserve(size);
+
+        // 初始化, 每一个parent[i]指向自己, 表示每一个元素自己自成一个集合
+        for(int i = 0; i < size; i++)
+        {
+            parent.emplace_back(i);
+            rank.emplace_back(1);
+        }
+    }
+
+    int getSize() override
+    {
+        return parent.size();
+    }
+
+    // 查看元素p和元素q是否所属一个集合
+    // O(h)复杂度, h为树的高度
+    bool isConnected(int p, int q) override
+    {
+        return find(p) == find(q);
+    }
+
+    // 合并元素p和元素q所属的集合
+    // O(h)复杂度, h为树的高度
+    void unionElement(int p, int q) override
+    {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if(pRoot == qRoot)
+        {
+            return;
+        }
+
+        // 根据两个元素所在树的rank不同判断合并方向
+        // 将rank低的集合合并到rank高的集合上
+        if(rank[pRoot] < rank[qRoot])
+        {
+            parent[pRoot] = qRoot;
+        }
+        else if(rank[pRoot] > rank[qRoot])
+        {
+            parent[qRoot] = pRoot;
+        }
+        else
+        {
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1; // 此时, 我维护rank的值
+        }
+    }
+};
+
+#endif
---
+++ 24-Union-Find/06-Path-Compression/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <ctime>
+#include "UnionFind1.h"
+#include "UnionFind2.h"
+#include "UnionFind3.h"
+#include "UnionFind4.h"
+#include "UnionFind5.h"
+
+double testUF(UF& uf, int m)
+{
+    int size = uf.getSize();
+    std::uniform_int_distribution<int> u(0, size - 1);
+    std::default_random_engine e(time(0));
+
+    auto start = std::chrono::steady_clock::now();
+
+    for(int i = 0; i < m; i++)
+    {
+        int a = u(e);
+        int b = u(e);
+        uf.unionElement(a, b);
+    }
+
+    for(int i = 0; i < m; i++)
+    {
+        int a = u(e);
+        int b = u(e);
+        uf.isConnected(a, b);
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time = end - start;
+
+    return time.count();
+}
+
+int main()
+{
+    // int size = 100000;
+    // int m = 10000;
+
+    int size = 10000000;
+    int m = 10000000;
+
+    // UnionFind1 uf1(size);
+    // std::cout << "UnionFind1 : " << testUF(uf1, m) << " s" << std::endl;
+
+    // UnionFind2 uf2(size);
+    // std::cout << "UnionFind2 : " << testUF(uf2, m) << " s" << std::endl;
+
+    UnionFind3 uf3(size);
+    std::cout << "UnionFind3 : " << testUF(uf3, m) << " s" << std::endl;
+
+    UnionFind4 uf4(size);
+    std::cout << "UnionFind4 : " << testUF(uf4, m) << " s" << std::endl;
+
+    UnionFind5 uf5(size);
+    std::cout << "UnionFind5 : " << testUF(uf5, m) << " s" << std::endl;
+
+    return 0;
+}
