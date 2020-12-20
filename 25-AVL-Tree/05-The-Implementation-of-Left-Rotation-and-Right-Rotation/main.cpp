039504df4d35b59ccbab8502cf7c4f4532972b58
yinweijie <ywj123450@gmail.com>
2020-12-21 01:06:51 +0800

    2505 completed

---
+++ 25-AVL-Tree/05-The-Implementation-of-Left-Rotation-and-Right-Rotation/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <chrono>
+#include <cctype>
+#include "Map.h"
+#include "BSTMap.h"
+#include "AVLTree.h"
+
+using namespace std;
+
+// 参考：https://en.cppreference.com/w/cpp/string/byte/tolower
+std::string str_tolower(std::string s)
+{
+    std::transform(s.begin(), s.end(), s.begin(), 
+                   [](unsigned char c){ return std::tolower(c); }
+                  );
+    return s;
+}
+
+void calcWords(AVLTree<string, int>& map, string str)
+{
+    // ifstream fin("test.txt");
+    ifstream fin(str);
+    string text;
+
+    while(getline(fin, text))
+    {
+        // 参考：https://www.zhihu.com/question/36642771/answer/865135551
+        char* token = strtok(const_cast<char*>(text.data()), " ");
+        while(token != NULL)
+        {
+            string word = str_tolower(token);
+            if(map.contains(word))
+            {
+                map.set(word, map.get(word) + 1);
+            }
+            else
+            {
+                map.add(word, 1);
+            }
+            token = strtok(NULL, " ");
+        }
+    }
+
+    cout << "Total different words: " << map.getSize() << endl;
+    cout << "Frequency of PRIDE: " << map.get("pride") << endl;
+    cout << "Frequency of PREJUDICE: " << map.get("prejudice") << endl;
+
+    cout << "is BST: " << boolalpha << map.isBST() << endl;
+    cout << "is Balanced: " << boolalpha << map.isBalanced() << endl;
+}
+
+int main()
+{
+    AVLTree<string, int> AVL;
+
+    calcWords(AVL, "pride-and-prejudice.txt");
+
+    return 0;
+}
