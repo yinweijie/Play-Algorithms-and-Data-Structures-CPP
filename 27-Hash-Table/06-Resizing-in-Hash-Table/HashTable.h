4a1cf920a5ab054d4d8f06721c02b1f98f435682
yinweijie <ywj123450@gmail.com>
2020-12-28 21:17:00 +0800

    2706 completed

---
+++ 27-Hash-Table/06-Resizing-in-Hash-Table/HashTable.h
@@ -0,0 +1,132 @@
+#ifndef __HASHTABLE_H__
+#define __HASHTABLE_H__
+
+#include <map>
+#include <vector>
+#include <functional>
+#include <stdexcept>
+#include <sstream>
+#include <utility>
+
+using namespace std;
+
+template <typename K, typename V>
+class HashTable
+{
+private:
+    static constexpr int upperTol = 10;
+    static constexpr int lowerTol = 2;
+    static constexpr int initCapacity = 7;
+
+    int size;
+    int M;
+    vector<map<K, V>> hashtable;
+
+    void resize(int newM)
+    {
+        vector<map<K, V>> newHashtable(newM);
+
+        int oldM = M;
+        M = newM;
+        for(int i = 0; i < oldM; i++)
+        {
+            map<K, V>& mymap = hashtable[i];
+            for(auto[key, value] : mymap)
+            {
+                newHashtable[hashFunc(key)][key] = value;
+            }
+        }
+
+        hashtable = std::move(newHashtable);
+    }
+
+public:
+    HashTable(int M_) : size(0), M(M_)
+    {
+        hashtable.resize(M);
+    }
+
+    HashTable() : HashTable(initCapacity) { }
+
+    size_t hashFunc(K key)
+    {
+        return (hash<K>()(key)) % M;
+    }
+
+    int getSize()
+    {
+        return size;
+    }
+
+    void add(K key, V value)
+    {
+        map<K, V>& mymap = hashtable[hashFunc(key)];
+
+        if(mymap.find(key) != mymap.end())
+        {
+            mymap[key] = value;
+        }
+        else
+        {
+            mymap[key] = value;
+            size++;
+
+            if(size >= M * upperTol)
+            {
+                resize(2 * M);
+            }
+        }
+    }
+
+    V remove(K key)
+    {
+        map<K, V>& mymap = hashtable[hashFunc(key)];
+
+        auto it = mymap.find(key);
+        if(it == mymap.end())
+        {
+            ostringstream os;
+            os << key << " does not exist.";
+            throw std::invalid_argument(os.str());
+        }
+
+        V ret = it->second;
+        mymap.erase(key);
+        size--;
+
+        if(size < M * lowerTol && M / 2 >= initCapacity)
+        {
+            resize(M / 2);
+        }
+
+        return ret;
+    }
+
+    void set(K key, V value)
+    {
+        map<K, V>& mymap = hashtable[hashFunc(key)];
+
+        auto it = mymap.find(key);
+        if(it == mymap.end())
+        {
+            ostringstream os;
+            os << key << " does not exist.";
+            throw std::invalid_argument(os.str());
+        }
+
+        it->second = value;
+    }
+
+    bool contains(K key)
+    {
+        map<K, V>& mymap = hashtable[hashFunc(key)];
+        return mymap.find(key) != mymap.end();
+    }
+
+    V get(K key)
+    {
+        return hashtable[hashFunc(key)].find(key)->second;
+    }
+};
+
+#endif
---
+++ 27-Hash-Table/06-Resizing-in-Hash-Table/main.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <chrono>
+#include <cctype>
+#include <exception>
+#include "BST.h"
+#include "AVLTree.h"
+#include "RBTree.h"
+#include "HashTable.h"
+
+using namespace std;
+
+// 参考：https://en.cppreference.com/w/cpp/string/byte/tolower
+std::string str_tolower(std::string s)
+{
+    std::transform(s.begin(), s.end(), s.begin(), 
+                   [](unsigned char c){ return std::tolower(c); }
+                  );
+    return s;
+}
+
+void calcWords(AVLTree<string, int>& map, string fileName)
+{
+    // ifstream fin("test.txt");
+    ifstream fin(fileName);
+    string text;
+
+    while(getline(fin, text))
+    {
+        // 参考：https://www.zhihu.com/question/36642771/answer/865135551
+        char* token = strtok(const_cast<char*>(text.data()), " ");
+        while(token != NULL)
+        {
+            string word = str_tolower(token);
+            if(map.contains(word))
+            {
+                map.set(word, map.get(word) + 1);
+            }
+            else
+            {
+                map.add(word, 1);
+            }
+            token = strtok(NULL, " ");
+        }
+    }
+
+    cout << "Total different words: " << map.getSize() << endl;
+    cout << "Frequency of PRIDE: " << map.get("pride") << endl;
+    cout << "Frequency of PREJUDICE: " << map.get("prejudice") << endl;
+
+    cout << "is BST: " << boolalpha << map.isBST() << endl;
+    cout << "is Balanced: " << boolalpha << map.isBalanced() << endl;
+}
+
+void wordToVector(vector<string>& words, string fileName)
+{
+    ifstream fin(fileName);
+    string text;
+
+    while(getline(fin, text))
+    {
+        // 参考：https://www.zhihu.com/question/36642771/answer/865135551
+        char* token = strtok(const_cast<char*>(text.data()), " ");
+        while(token != NULL)
+        {
+            string word = str_tolower(token);
+            words.push_back(word);
+            token = strtok(NULL, " ");
+        }
+    }
+}
+
+int main()
+{
+    vector<string> words;
+    wordToVector(words, "pride-and-prejudice.txt");
+
+    cout << "Total words: " << words.size() << endl;
+
+    // std::sort(words.begin(), words.end());
+
+    // Test BST
+
+    auto start = std::chrono::steady_clock::now();
+
+    BST<string, int> bst;
+    for(auto word : words)
+    {
+        if(bst.contains(word))
+        {
+            bst.set(word, bst.get(word) + 1);
+        }
+        else
+        {
+            bst.add(word, 1);
+        }
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    chrono::duration<double> time = end - start;
+    cout << "BST: " << time.count() << " s" << endl;
+
+    // Test AVLTree
+
+    start = std::chrono::steady_clock::now();
+
+    AVLTree<string, int> avlTree;
+    for(auto word : words)
+    {
+        if(avlTree.contains(word))
+        {
+            avlTree.set(word, avlTree.get(word) + 1);
+        }
+        else
+        {
+            avlTree.add(word, 1);
+        }
+    }
+
+    end = std::chrono::steady_clock::now();
+    time = end - start;
+    cout << "AVLTree: " << time.count() << " s" << endl;
+
+    // Test RBTree
+
+    start = std::chrono::steady_clock::now();
+
+    RBTree<string, int> rbTree;
+    for(auto word : words)
+    {
+        if(rbTree.contains(word))
+        {
+            rbTree.set(word, rbTree.get(word) + 1);
+        }
+        else
+        {
+            rbTree.add(word, 1);
+        }
+    }
+
+    end = std::chrono::steady_clock::now();
+    time = end - start;
+    cout << "RBTree: " << time.count() << " s" << endl;
+
+    // Test HashTable
+
+    start = std::chrono::steady_clock::now();
+
+    // HashTable<string, int> ht;
+    HashTable<string, int> ht(131071);
+    for(auto word : words)
+    {
+        if(ht.contains(word))
+        {
+            ht.set(word, ht.get(word) + 1);
+        }
+        else
+        {
+            ht.add(word, 1);
+        }
+    }
+
+    end = std::chrono::steady_clock::now();
+    time = end - start;
+    cout << "HashTable: " << time.count() << " s" << endl;
+
+    return 0;
+}
